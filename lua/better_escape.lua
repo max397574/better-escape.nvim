49a17401668f531bf27ac66f4c4d23b5a68d0d31
max397574 <max.heinzer397@gmx.ch>
2021-09-22 14:35:47 +0200

    feat(plugin): ✨allow mappings with same letters

--- lua/better_escape.lua
+++ lua/better_escape.lua
@@ -3,8 +3,8 @@ local M = {}
 local previuos_chars = {}
 vim.g.better_escape_flag = false
 local settings = {
-	mapping = "jk",
-	timeout = 200
+  mapping = "jj",
+  timeout = 200
 }
 
 local t = function(str)
@@ -17,46 +17,42 @@ function M.check_charaters()
   local timeout = settings.timeout
   table.insert(previuos_chars, vim.v.char)
   local prev_char = previuos_chars[#previuos_chars - 1] or ""
-
-  if vim.v.char == first_char then
-    vim.g.better_escape_flag = true
-    local timer = vim.loop.new_timer()
-    timer:start(timeout, 0, function()
+  if vim.v.char == second_char and prev_char == first_char then
+    if vim.g.better_escape_flag then
+      vim.api.nvim_feedkeys(t "<BS><BS><Esc>", "n", false)
+    else
       vim.g.better_escape_flag = false
-    end)
-  else
-
-    if vim.v.char == second_char and prev_char == first_char then
-      if vim.g.better_escape_flag then
-        vim.api.nvim_feedkeys(t "<BS><BS><Esc>", "n", false)
-      else
-        vim.g.better_escape_flag = false
-      end
-      previuos_chars = {}
     end
-
+    previuos_chars = {}
+  else if vim.v.char == first_char then
+      vim.g.better_escape_flag = true
+      local timer = vim.loop.new_timer()
+      timer:start(timeout, 0, function()
+        vim.g.better_escape_flag = false
+      end)
+    else
+    end
   end
 end
 
-local function validate_settings()
-  if type(settings.mapping) ~= "string" then
-    print("Error(better-escape.nvim): Mapping must be a string.")
+  local function validate_settings()
+    if type(settings.mapping) ~= "string" then
+      print("Error(better-escape.nvim): Mapping must be a string.")
+    end
+    if #settings.mapping ~= 2 then
+      print("Error(better-escape.nvim): Mapping must be 2 keys.")
+    end
+    if type(settings.timeout) ~= "number" then
+      print("Error(better-escape.nvim): Timeout must be a number.")
+    end
   end
-  if #settings.mapping ~= 2 then
-    print("Error(better-escape.nvim): Mapping must be 2 keys.")
-  end
-  if type(settings.timeout) ~= "number" then
-    print("Error(better-escape.nvim): Timeout must be a number.")
-  end
-end
 
-function M.setup(update)
-  -- settings = setmetatable(update, { __index = settings })
-  settings = vim.tbl_deep_extend("force", settings, update or {})
+  function M.setup(update)
+    -- settings = setmetatable(update, { __index = settings })
+    settings = vim.tbl_deep_extend("force", settings, update or {})
 
-	vim.cmd [[au InsertCharPre * lua require"better_escape".check_charaters()]]
-  validate_settings()
+    vim.cmd [[au InsertCharPre * lua require"better_escape".check_charaters()]]
+    validate_settings()
 end
 
 return M
-
