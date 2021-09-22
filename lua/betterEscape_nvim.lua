c9acf0435eb3d0db0ee2205407b80dfdbf8932ba
max397574 <max.heinzer397@gmx.ch>
2021-09-22 09:47:52 +0200

    feat(plugin): ✨made timeout work a little bit

--- lua/betterEscape_nvim.lua
+++ lua/betterEscape_nvim.lua
@@ -3,7 +3,7 @@ local M = {}
 local settings = {}
 
 settings.mapping = "jk"
-settings.timeout = "150"
+settings.timeout = "300"
 
 function M.setup(update)
   settings = setmetatable(update, { __index = settings })
@@ -15,26 +15,36 @@ end
 
 local previuosly_typed_chars = {}
 
+BetterEscape_nvim_time = 1000
+
 function M.check_charaters()
   local first_char = string.sub(settings.mapping,1,1)
   local second_char = string.sub(settings.mapping,2,2)
+  local timeout = (tonumber(settings.timeout))/10000
   table.insert(previuosly_typed_chars, vim.v.char)
   local prev_char = previuosly_typed_chars[#previuosly_typed_chars-1] or ""
-
-  if vim.v.char == second_char and prev_char == first_char then
-    vim.fn.feedkeys(t('<Esc>').."xhx", 'i')
-    previuosly_typed_chars = {}
+  if vim.v.char == first_char then
+    BetterEscape_nvim_time = os.clock()
+  else
+    if vim.v.char == second_char and prev_char == first_char then
+      local time_difference = os.clock() - BetterEscape_nvim_time
+      if time_difference < timeout then
+        vim.fn.feedkeys(t('<Esc>').."xhx", 'i')
+      else
+        BetterEscape_nvim_time = 1000
+      end
+      previuosly_typed_chars = {}
+    end
   end
-  vim.loop.new_timer(settings.timeout)
-  previuosly_typed_chars = {}
+
 end
 
-local function create_mappings()
+local function create_autocmds()
   vim.cmd [[au InsertCharPre * lua require"betterEscape_nvim".check_charaters()]]
 end
 
 function M.init()
-  create_mappings()
+  create_autocmds()
 end
 
 
